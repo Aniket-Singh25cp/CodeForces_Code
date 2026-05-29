fac469c291b472e0e6d1f1e55429addb7a1dbc2f
Aniket Kumar Singh <aniketyt2503@gmail.com>
2026-05-29 09:19:37 +0530

    add problem-2

---
+++ A2_OJ_Ladder-1300/young_physicist.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin>>n;
+    int xSum = 0, ySum = 0, zSum = 0;
+    while(n--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        xSum += x;
+        ySum += y;
+        zSum += z;
+    }
+    if(xSum==0 && ySum==0 && zSum==0){
+        cout<<"YES"<<"\n";
+    }else{
+        cout<<"NO"<<"\n";
+    }
+    return 0;
+}
