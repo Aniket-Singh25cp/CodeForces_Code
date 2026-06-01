23cf378542268faeadbefdaaff243a5ec721fbeb
Aniket Kumar Singh <aniketyt2503@gmail.com>
2026-06-01 21:58:23 +0530

    add problem-19

---
+++ CF_Beginner/word_capitalization.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s; cin>>s;
+    if(s[0]>='a' && s[0]<='z'){
+        s[0] -= 32;
+    }
+    cout<<s<<"\n";
+    return 0;
+}
