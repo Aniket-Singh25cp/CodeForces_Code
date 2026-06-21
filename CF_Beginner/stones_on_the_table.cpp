ec1ef0ad07ea455b898701d5388caeeaadbfe962
Aniket Kumar Singh <aniketyt2503@gmail.com>
2026-06-21 15:56:25 +0530

    add problem-21

---
+++ CF_Beginner/stones_on_the_table.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin>>n;
+    string s; cin>>s;
+    int c = 0;
+    for(int i=0;i<s.length()-1;++i){
+        if(s[i]==s[i+1]){
+            c++;
+        }
+    }
+    cout<<c<<"\n";
+    return 0;
+}
